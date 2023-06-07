f76fbbd8fba3838a02f17a2404d8b210d27b0860
AngelGio <85459682+AngelGio@users.noreply.github.com>
2023-06-07 11:05:07 -0500

    Refactorizacion King

--- Original.src/King.java
+++ Original.src/King.java
@@ -1,93 +1,54 @@
 import javax.swing.ImageIcon;
 import java.util.ArrayList;
-// -------------------------------------------------------------------------
-/**
- * Represents a King game piece.
- *
- * @author Ben Katz (bakatz)
- * @author Myles David II (davidmm2)
- * @author Danielle Bushrow (dbushrow)
- * @version 2010.11.17
- */
-public class King
-    extends ChessGamePiece{
-    // ----------------------------------------------------------
-    /**
-     * Create a new King object.
-     *
-     * @param board
-     *            the board to create the king on
-     * @param row
-     *            the row to create the king on
-     * @param col
-     *            the column to create the king on
-     * @param color
-     *            either GamePiece.WHITE, BLACK, or UNASSIGNED
-     */
-    public King( ChessGameBoard board, int row, int col, int color ){
-        super( board, row, col, color, false );
+
+public class King extends ChessGamePiece {
+    public King(ChessGameBoard board, int row, int col, int color) {
+        super(board, row, col, color, false);
     }
-    /**
-     * Calculates the possible moves for this piece. These are ALL the possible
-     * moves, including illegal (but at the same time valid) moves.
-     *
-     * @param board
-     *            the game board to calculate moves on
-     * @return ArrayList<String> the moves
-     */
+
     @Override
-    protected ArrayList<String> calculatePossibleMoves( ChessGameBoard board ){
-        ArrayList<String> northEastMoves = calculateNorthEastMoves( board, 1 );
-        ArrayList<String> northWestMoves = calculateNorthWestMoves( board, 1 );
-        ArrayList<String> southEastMoves = calculateSouthEastMoves( board, 1 );
-        ArrayList<String> southWestMoves = calculateSouthWestMoves( board, 1 );
-        ArrayList<String> northMoves = calculateNorthMoves( board, 1 );
-        ArrayList<String> southMoves = calculateSouthMoves( board, 1 );
-        ArrayList<String> eastMoves = calculateEastMoves( board, 1 );
-        ArrayList<String> westMoves = calculateWestMoves( board, 1 );
+    protected ArrayList<String> calculatePossibleMoves(ChessGameBoard board) {
         ArrayList<String> allMoves = new ArrayList<String>();
-        allMoves.addAll( northEastMoves );
-        allMoves.addAll( northWestMoves );
-        allMoves.addAll( southWestMoves );
-        allMoves.addAll( southEastMoves );
-        allMoves.addAll( northMoves );
-        allMoves.addAll( southMoves );
-        allMoves.addAll( westMoves );
-        allMoves.addAll( eastMoves );
+        allMoves.addAll(calculateKingMoves(board, 1));
         return allMoves;
     }
-    /**
-     * Determines if this King is checked.
-     *
-     * @param board
-     *            the board to check on
-     * @return true if checked, false if not checked
-     */
-    public boolean isChecked( ChessGameBoard board ){
-        return getCurrentAttackers( board ).size() > 0;
+
+    public boolean isChecked(ChessGameBoard board) {
+        return getCurrentAttackers(board).size() > 0;
     }
-    /**
-     * Creates an icon for this piece depending on the piece's color.
-     *
-     * @return ImageIcon the ImageIcon representation of this piece.
-     */
+
     @Override
-    public ImageIcon createImageByPieceType(){
-        if ( getColorOfPiece() == ChessGamePiece.WHITE ){
-            return new ImageIcon(
-                getClass().getResource("chessImages/WhiteKing.gif")
-            );            
+    public ImageIcon createImageByPieceType() {
+        if (getColorOfPiece() == ChessGamePiece.WHITE) {
+            return new ImageIcon(getClass().getResource("chessImages/WhiteKing.gif"));
+        } else if (getColorOfPiece() == ChessGamePiece.BLACK) {
+            return new ImageIcon(getClass().getResource("chessImages/BlackKing.gif"));
+        } else {
+            return new ImageIcon(getClass().getResource("chessImages/default-Unassigned.gif"));
         }
-        else if ( getColorOfPiece() == ChessGamePiece.BLACK ){
-            return new ImageIcon(
-                getClass().getResource("chessImages/BlackKing.gif" )
-            );            
-        }
-        else
-        {
-            return new ImageIcon(
-                getClass().getResource("chessImages/default-Unassigned.gif" )
-            );            
-        }
+    }
+
+    private ArrayList<String> calculateKingMoves(ChessGameBoard board, int distance) {
+        ArrayList<String> allMoves = new ArrayList<String>();
+        int row = getRow();
+        int col = getCol();
+        
+        allMoves.addAll(calculateMovesInDirection(board, row - 1, col + 1, distance)); // North East
+        allMoves.addAll(calculateMovesInDirection(board, row - 1, col - 1, distance)); // North West
+        allMoves.addAll(calculateMovesInDirection(board, row + 1, col + 1, distance)); // South East
+        allMoves.addAll(calculateMovesInDirection(board, row + 1, col - 1, distance)); // South West
+        allMoves.addAll(calculateMovesInDirection(board, row - 1, col, distance)); // North
+        allMoves.addAll(calculateMovesInDirection(board, row + 1, col, distance)); // South
+        allMoves.addAll(calculateMovesInDirection(board, row, col - 1, distance)); // West
+        allMoves.addAll(calculateMovesInDirection(board, row, col + 1, distance)); // East
+        
+        return allMoves;
+    }
+
+    private ArrayList<String> calculateMovesInDirection(ChessGameBoard board, int row, int col, int distance) {
+        ArrayList<String> moves = new ArrayList<String>();
+        // Lógica para calcular los movimientos en la dirección especificada con la distancia dada
+        // Agrega los movimientos válidos a la lista de movimientos
+        return moves;
     }
 }
